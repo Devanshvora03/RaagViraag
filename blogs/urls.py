62e61f91af9b8a9012eae0e6d55cba7e83e1a457
Devanshvora03 <131452370+Devanshvora03@users.noreply.github.com>
2024-01-08 21:07:57 +0530

    CSS Changes

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,11 @@
 from .views import *
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='index'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('categories/', categories, name='categories'),
